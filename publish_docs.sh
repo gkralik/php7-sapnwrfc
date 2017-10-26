c337e24a4a3d8c4199cb91c57b87916c8274e2fe
Gregor Kralik <g.kralik@gmail.com>
2017-10-26 16:14:19 +0200

    add script to publish docs
    generates the docs, moves them to the gh-pages branch and
    pushes the changes

---
+++ publish_docs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# build the docs
+cd docs
+make clean html
+cd ..
+
+# switch to branch gh-pages and update the docs
+git checkout gh-pages
+
+# remove everything _except_: .git and the docs folder (which contains
+# the generated docs)
+find . -maxdepth 1 ! -path . -and ! -name .git -and ! -name docs -exec rm -r {} \;
+
+# make sure the .nojekyll file is there
+touch .nojekyll
+
+# move the generated docs
+mv ./docs/_build/html/* ./
+rm -rf ./docs
+
+# add to repo
+git add -A
+git commit -m "updated docs"
+git push origin gh-pages
+
+# switch back to master
+git checkout master
