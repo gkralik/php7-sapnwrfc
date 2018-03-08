gkralik/php7-sapnwrfc

commit bfe868c64ab36522272e03e2d306ad9b23c93d9d

allow options for RemoteFunction::invoke()
second parameter is array of options.
currently only 'rtrim' is supported (defaults to true)
which helps with CHAR return parameters by removing
the right-padding before returning the values.

sapnwrfc.c
@@ -169,6 +169,7 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_RemoteFunction_invoke, 0, 1, IS_
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_RemoteFunction_invoke, 0, 1, IS_ARRAY, NULL, 0)
 #endif
     ZEND_ARG_ARRAY_INFO(0, parameters, 0)
+    ZEND_ARG_ARRAY_INFO(0, options, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_RemoteFunction_setParameterActive, 0, 0, 2)
@@ -494,7 +495,6 @@ PHP_METHOD(Connection, getFunction)
     func_intern->rfc_handle = intern->rfc_handle;
     func_intern->function_desc_handle = function_desc_handle;
     func_intern->name = zend_string_copy(function_name);
-
     add_property_str(return_value, "name", zend_string_copy(function_name));
 
     // get nr of parameters
@@ -678,15 +678,17 @@ PHP_METHOD(RemoteFunction, invoke)
     unsigned int i = 0;
     zval *in_parameters;
     HashTable *in_parameters_hash;
+    HashTable *options_hash = NULL;
     zend_string *tmp;
     zend_string *key;
     zval *val;
     zval retval;
     SAP_UC *parameter_name_u;
+    unsigned char rtrim_enabled = 1;
 
     zend_replace_error_handling(EH_THROW, sapnwrfc_function_exception_ce, NULL);
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS(), "a", &in_parameters) == FAILURE) {
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "a|h", &in_parameters, &options_hash) == FAILURE) {
         zend_replace_error_handling(EH_NORMAL, NULL, NULL);
         return;
     }
@@ -787,6 +789,15 @@ PHP_METHOD(RemoteFunction, invoke)
     // now get the return values
     array_init(return_value);
 
+    // check for rtrim option
+    if (options_hash) {
+        zval *rtrim;
+        if ((rtrim = zend_hash_str_find(options_hash, "rtrim", strlen("rtrim"))) != NULL) {
+            convert_to_boolean(rtrim);
+            rtrim_enabled = zend_is_true(rtrim);
+        }
+    }
+
     for (i = 0; i < intern->parameter_count; i++) {
         rc = RfcGetParameterDescByIndex(intern->function_desc_handle, i, &parameter_desc, &error_info);
         if (rc != RFC_OK) {
@@ -816,7 +827,7 @@ PHP_METHOD(RemoteFunction, invoke)
             case RFC_CHANGING:
             case RFC_TABLES:
                 tmp = sapuc_to_zend_string(parameter_desc.name);
-                retval = rfc_get_parameter_value(function_handle, intern->function_desc_handle, tmp);
+                retval = rfc_get_parameter_value(function_handle, intern->function_desc_handle, tmp, rtrim_enabled);
 
                 if (ZVAL_IS_NULL(&retval)) {
                     zend_string_release(tmp);

tests/041_invoke_returns_result.phpt
@@ -37,26 +37,26 @@ var_dump(is_array($result));
 var_dump(array_key_exists('ECHOSTRUCT', $result));
 var_dump(array_key_exists('RFCTABLE', $result));
 
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCCHAR1"]) == $importStruct["RFCCHAR1"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCCHAR2"]) == $importStruct["RFCCHAR2"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCCHAR4"]) == $importStruct["RFCCHAR4"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCFLOAT"]) == $importStruct["RFCFLOAT"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCINT1"]) == $importStruct["RFCINT1"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCINT2"]) == $importStruct["RFCINT2"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCINT4"]) == $importStruct["RFCINT4"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCDATA1"]) == $importStruct["RFCDATA1"]);
-var_dump(rtrim($result["ECHOSTRUCT"]["RFCDATA2"]) == $importStruct["RFCDATA2"]);
+var_dump($result["ECHOSTRUCT"]["RFCCHAR1"] == $importStruct["RFCCHAR1"]);
+var_dump($result["ECHOSTRUCT"]["RFCCHAR2"] == $importStruct["RFCCHAR2"]);
+var_dump($result["ECHOSTRUCT"]["RFCCHAR4"] == $importStruct["RFCCHAR4"]);
+var_dump($result["ECHOSTRUCT"]["RFCFLOAT"] == $importStruct["RFCFLOAT"]);
+var_dump($result["ECHOSTRUCT"]["RFCINT1"] == $importStruct["RFCINT1"]);
+var_dump($result["ECHOSTRUCT"]["RFCINT2"] == $importStruct["RFCINT2"]);
+var_dump($result["ECHOSTRUCT"]["RFCINT4"] == $importStruct["RFCINT4"]);
+var_dump($result["ECHOSTRUCT"]["RFCDATA1"] == $importStruct["RFCDATA1"]);
+var_dump($result["ECHOSTRUCT"]["RFCDATA2"] == $importStruct["RFCDATA2"]);
 
 var_dump(count($result["RFCTABLE"]) == 2);
-var_dump(rtrim($result["RFCTABLE"][1]["RFCFLOAT"]) == $importStruct["RFCFLOAT"] + 1);
-var_dump(rtrim($result["RFCTABLE"][1]["RFCINT1"]) == $importStruct["RFCINT1"] + 1);
-var_dump(rtrim($result["RFCTABLE"][1]["RFCINT2"]) == $importStruct["RFCINT2"] + 1);
-var_dump(rtrim($result["RFCTABLE"][1]["RFCINT4"]) == $importStruct["RFCINT4"] + 1);
+var_dump($result["RFCTABLE"][1]["RFCFLOAT"] == $importStruct["RFCFLOAT"] + 1);
+var_dump($result["RFCTABLE"][1]["RFCINT1"] == $importStruct["RFCINT1"] + 1);
+var_dump($result["RFCTABLE"][1]["RFCINT2"] == $importStruct["RFCINT2"] + 1);
+var_dump($result["RFCTABLE"][1]["RFCINT4"] == $importStruct["RFCINT4"] + 1);
 
-var_dump(rtrim($result["RFCTABLE"][1]["RFCCHAR1"]) == "X");
-//var_dump(rtrim($result["RFCTABLE"][1]["RFCCHAR4"]) == "SYS"); // system name
-var_dump(rtrim($result["RFCTABLE"][1]["RFCHEX3"]) == hex2bin("F1F2F3"));
-var_dump(rtrim($result["RFCTABLE"][1]["RFCCHAR2"]) == "YZ");
+var_dump($result["RFCTABLE"][1]["RFCCHAR1"] == "X");
+//var_dump($result["RFCTABLE"][1]["RFCCHAR4"] == "SYS"); // system name
+var_dump($result["RFCTABLE"][1]["RFCHEX3"] == hex2bin("F1F2F3"));
+var_dump($result["RFCTABLE"][1]["RFCCHAR2"] == "YZ");
 --EXPECT--
 bool(true)
 bool(true)

tests/085_returned_CHAR_is_not_right_trimmed_when_rtrim_option_disabled.phpt
@@ -1,0 +1,37 @@
+--TEST--
+Returned CHAR value is not right-trimmed when rtrim option disabled.
+--SKIPIF--
+<?php include("should_run_online_tests.inc"); ?>
+--FILE--
+<?php
+$config = include "sapnwrfc.config.inc";
+$c = new \SAPNWRFC\Connection($config);
+
+$params = [
+    "IMPORTSTRUCT" => [
+        'RFCFLOAT' => 1.23456789,
+        'RFCCHAR1' => 'A',
+        'RFCCHAR2' => 'BC',
+        'RFCCHAR4' => 'DEFG',
+        'RFCINT1' => 1,
+        'RFCINT2' => 2,
+        'RFCINT4' => 345,
+        'RFCHEX3' => 'fgh',
+        'RFCTIME' => '121120',
+        'RFCDATE' => '20140101',
+        'RFCDATA1' => '1DATA1',
+        'RFCDATA2' => 'DATA222',
+    ],
+];
+
+$f = $c->getFunction("STFC_STRUCTURE");
+$f->setParameterActive('RFCTABLE', false);
+
+$ret = $f->invoke($params, ['rtrim' => false]);
+
+var_dump($ret['ECHOSTRUCT']['RFCDATA1'] == str_pad('1DATA1', 50, ' ', STR_PAD_RIGHT));
+var_dump($ret['ECHOSTRUCT']['RFCDATA2'] == str_pad('DATA222', 50, ' ', STR_PAD_RIGHT));
+
+--EXPECT--
+bool(true)
+bool(true)

.gitignore
@@ -35,3 +35,4 @@ run-tests.php
 *.la
 *.lo
 *.trc
+playground/

docs/.gitignore
@@ -1,1 +1,2 @@
 _build/*
+todo.rst

docs/sapnwrfc.stubs.php
@@ -187,11 +187,12 @@ class RemoteFunction
      * Invoke the RFC function.
      *
      * @param array $parameters Function parameters.
-     * @returns array Return value from the backend.
+     * @param array $options Additional invoke options. Only 'rtrim' supported for now.
+     * @return array Return value from the backend.
      *
      * @throws FunctionCallException if any error occurs during execution.
      */
-    public function invoke(array $parameters = [])
+    public function invoke(array $parameters, array $options = [])
     {
     }
 

rfc_parameters.c
@@ -683,7 +683,7 @@ rfc_set_value_return_t rfc_set_parameter_value(RFC_FUNCTION_HANDLE function_hand
     return ret;
 }
 
-zval rfc_get_char_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len)
+zval rfc_get_char_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len, unsigned char rtrim_enabled)
 {
     RFC_RC rc = RFC_OK;
     RFC_ERROR_INFO error_info;
@@ -704,7 +704,7 @@ zval rfc_get_char_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len)
         return value;
     }
 
-    value = sapuc_to_zval_len(buf, len);
+    value = sapuc_to_zval_len_ex(buf, len, rtrim_enabled);
     free(buf);
 
     return value;
@@ -783,7 +783,7 @@ zval rfc_get_byte_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len)
     return value;
 }
 
-zval rfc_get_table_value(DATA_CONTAINER_HANDLE h, SAP_UC *name)
+zval rfc_get_table_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned char rtrim_enabled)
 {
     RFC_RC rc = RFC_OK;
     RFC_ERROR_INFO error_info;
@@ -810,7 +810,7 @@ zval rfc_get_table_value(DATA_CONTAINER_HANDLE h, SAP_UC *name)
         RfcMoveTo(h, i, NULL);
         line_handle = RfcGetCurrentRow(h, NULL);
 
-        line_value = rfc_get_table_line(line_handle, zname);
+        line_value = rfc_get_table_line(line_handle, zname, rtrim_enabled);
         if (ZVAL_IS_NULL(&line_value)) {
             // error; exception has been thrown
             ZVAL_NULL(&value);
@@ -944,7 +944,7 @@ zval rfc_get_int2_value(DATA_CONTAINER_HANDLE h, SAP_UC *name)
     return value;
 }
 
-zval rfc_get_structure_value(DATA_CONTAINER_HANDLE h, SAP_UC *name)
+zval rfc_get_structure_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned char rtrim_enabled)
 {
     RFC_RC rc = RFC_OK;
     RFC_ERROR_INFO error_info;
@@ -963,7 +963,7 @@ zval rfc_get_structure_value(DATA_CONTAINER_HANDLE h, SAP_UC *name)
         return value;
     }
 
-    value = rfc_get_table_line(line_handle, zname);
+    value = rfc_get_table_line(line_handle, zname, rtrim_enabled);
     zend_string_release(zname);
 
     return value;
@@ -1057,7 +1057,7 @@ zval rfc_get_xstring_value(DATA_CONTAINER_HANDLE h, SAP_UC *name)
     return value;
 }
 
-zval rfc_get_table_line(RFC_STRUCTURE_HANDLE line, zend_string *param_name)
+zval rfc_get_table_line(RFC_STRUCTURE_HANDLE line, zend_string *param_name, unsigned char rtrim_enabled)
 {
     RFC_RC rc = RFC_OK;
     RFC_ERROR_INFO error_info;
@@ -1096,7 +1096,7 @@ zval rfc_get_table_line(RFC_STRUCTURE_HANDLE line, zend_string *param_name)
             return value;
         }
 
-        field_value = rfc_get_field_value(line, field_desc);
+        field_value = rfc_get_field_value(line, field_desc, rtrim_enabled);
         if (ZVAL_IS_NULL(&field_value)) {
             // error; exception has been thrown
             ZVAL_NULL(&value);
@@ -1111,7 +1111,7 @@ zval rfc_get_table_line(RFC_STRUCTURE_HANDLE line, zend_string *param_name)
     return value;
 }
 
-zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc)
+zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc, unsigned char rtrim_enabled)
 {
     RFC_RC rc = RFC_OK;
     RFC_ERROR_INFO error_info;
@@ -1121,7 +1121,7 @@ zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc)
 
     switch(field_desc.type) {
         case RFCTYPE_CHAR:
-            value = rfc_get_char_value(h, field_desc.name, field_desc.nucLength);
+            value = rfc_get_char_value(h, field_desc.name, field_desc.nucLength, rtrim_enabled);
             break;
         case RFCTYPE_DATE:
             value = rfc_get_date_value(h, field_desc.name);
@@ -1143,7 +1143,7 @@ zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc)
                 return value;
             }
 
-            value = rfc_get_table_value(table_handle, field_desc.name);
+            value = rfc_get_table_value(table_handle, field_desc.name, rtrim_enabled);
             break;
         case RFCTYPE_NUM:
             value = rfc_get_num_value(h, field_desc.name, field_desc.nucLength);
@@ -1162,7 +1162,7 @@ zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc)
             value = rfc_get_int2_value(h, field_desc.name);
             break;
         case RFCTYPE_STRUCTURE:
-            value = rfc_get_structure_value(h, field_desc.name);
+            value = rfc_get_structure_value(h, field_desc.name, rtrim_enabled);
             break;
         case RFCTYPE_STRING:
             value = rfc_get_string_value(h, field_desc.name);
@@ -1182,7 +1182,8 @@ zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc)
 
 zval rfc_get_parameter_value(RFC_FUNCTION_HANDLE function_handle,
                                                RFC_FUNCTION_DESC_HANDLE function_desc_handle,
-                                               zend_string *name)
+                                               zend_string *name,
+                                               unsigned char rtrim_enabled)
 {
     RFC_RC rc = RFC_OK;
     RFC_ERROR_INFO error_info;
@@ -1203,7 +1204,7 @@ zval rfc_get_parameter_value(RFC_FUNCTION_HANDLE function_handle,
 
     switch(parameter_desc.type) {
         case RFCTYPE_CHAR:
-            value = rfc_get_char_value(function_handle, parameter_name_u, parameter_desc.nucLength);
+            value = rfc_get_char_value(function_handle, parameter_name_u, parameter_desc.nucLength, rtrim_enabled);
             break;
         case RFCTYPE_DATE:
             value = rfc_get_date_value(function_handle, parameter_name_u);
@@ -1223,7 +1224,7 @@ zval rfc_get_parameter_value(RFC_FUNCTION_HANDLE function_handle,
 
                 ZVAL_NULL(&value);
             }
-            value = rfc_get_table_value(table_handle, parameter_name_u);
+            value = rfc_get_table_value(table_handle, parameter_name_u, rtrim_enabled);
             break;
         case RFCTYPE_NUM:
             value = rfc_get_num_value(function_handle, parameter_name_u, parameter_desc.nucLength);
@@ -1242,7 +1243,7 @@ zval rfc_get_parameter_value(RFC_FUNCTION_HANDLE function_handle,
             value = rfc_get_int2_value(function_handle, parameter_name_u);
             break;
         case RFCTYPE_STRUCTURE:
-            value = rfc_get_structure_value(function_handle, parameter_name_u);
+            value = rfc_get_structure_value(function_handle, parameter_name_u, rtrim_enabled);
             break;
         case RFCTYPE_STRING:
             value = rfc_get_string_value(function_handle, parameter_name_u);

rfc_parameters.h
@@ -35,25 +35,26 @@ rfc_set_value_return_t rfc_set_parameter_value(RFC_FUNCTION_HANDLE function_hand
                                zval *value);
 
 
-zval rfc_get_char_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len);
+zval rfc_get_char_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len, unsigned char rtrim_enabled);
 zval rfc_get_date_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 zval rfc_get_time_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 zval rfc_get_byte_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len);
-zval fc_get_table_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
+zval fc_get_table_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned char rtrim_enabled);
 zval rfc_get_num_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned len);
 zval rfc_get_float_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 zval rfc_get_int_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 zval rfc_get_int1_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 zval rfc_get_int2_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
-zval rfc_get_structure_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
+zval rfc_get_structure_value(DATA_CONTAINER_HANDLE h, SAP_UC *name, unsigned char rtrim_enabled);
 zval rfc_get_string_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 zval rfc_get_xstring_value(DATA_CONTAINER_HANDLE h, SAP_UC *name);
 
-zval rfc_get_table_line(RFC_STRUCTURE_HANDLE line, zend_string *param_name);
-zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc);
+zval rfc_get_table_line(RFC_STRUCTURE_HANDLE line, zend_string *param_name, unsigned char rtrim_enabled);
+zval rfc_get_field_value(RFC_STRUCTURE_HANDLE h, RFC_FIELD_DESC field_desc, unsigned char rtrim_enabled);
 
 zval rfc_get_parameter_value(RFC_FUNCTION_HANDLE function_handle,
                                RFC_FUNCTION_DESC_HANDLE function_desc_handle,
-                               zend_string *name);
+                               zend_string *name,
+                               unsigned char rtrim_enabled);
 
 #endif /* _RFC_PARAMETERS_H */

commit 4bcc3ed1b0551fbba48c7988e6f45debee9e49e7

disable rtrim option by default
this fixes a BC break introduced in bfe868c.

refs #49

sapnwrfc.c
@@ -682,7 +682,7 @@ PHP_METHOD(RemoteFunction, invoke)
     zval *val;
     zval retval;
     SAP_UC *parameter_name_u;
-    unsigned char rtrim_enabled = 1;
+    unsigned char rtrim_enabled = 0;
 
     zend_replace_error_handling(EH_THROW, sapnwrfc_function_exception_ce, NULL);
 

tests/041_invoke_returns_result.phpt
@@ -44,8 +44,8 @@ var_dump($result["ECHOSTRUCT"]["RFCFLOAT"] == $importStruct["RFCFLOAT"]);
 var_dump($result["ECHOSTRUCT"]["RFCINT1"] == $importStruct["RFCINT1"]);
 var_dump($result["ECHOSTRUCT"]["RFCINT2"] == $importStruct["RFCINT2"]);
 var_dump($result["ECHOSTRUCT"]["RFCINT4"] == $importStruct["RFCINT4"]);
-var_dump($result["ECHOSTRUCT"]["RFCDATA1"] == $importStruct["RFCDATA1"]);
-var_dump($result["ECHOSTRUCT"]["RFCDATA2"] == $importStruct["RFCDATA2"]);
+var_dump($result["ECHOSTRUCT"]["RFCDATA1"] == str_pad($importStruct["RFCDATA1"], 50, ' ', STR_PAD_RIGHT));
+var_dump($result["ECHOSTRUCT"]["RFCDATA2"] == str_pad($importStruct["RFCDATA2"], 50, ' ', STR_PAD_RIGHT));
 
 var_dump(count($result["RFCTABLE"]) == 2);
 var_dump($result["RFCTABLE"][1]["RFCFLOAT"] == $importStruct["RFCFLOAT"] + 1);

tests/084_rtrim_disabled_by_default.phpt
@@ -1,5 +1,5 @@
 --TEST--
-rtrim is enabled by default.
+rtrim is disabled by default.
 --SKIPIF--
 <?php include("should_run_online_tests.inc"); ?>
 --FILE--
@@ -28,8 +28,8 @@ $f = $c->getFunction("STFC_STRUCTURE");
 $f->setParameterActive('RFCTABLE', false);
 $ret = $f->invoke($params);
 
-var_dump($ret['ECHOSTRUCT']['RFCDATA1'] == '1DATA1');
-var_dump($ret['ECHOSTRUCT']['RFCDATA2'] == 'DATA222');
+var_dump($ret['ECHOSTRUCT']['RFCDATA1'] == str_pad('1DATA1', 50, ' ', STR_PAD_RIGHT));
+var_dump($ret['ECHOSTRUCT']['RFCDATA2'] == str_pad('DATA222', 50, ' ', STR_PAD_RIGHT));
 
 --EXPECT--
 bool(true)

tests/085_returned_CHAR_is_not_right_trimmed_when_rtrim_option_disabled.phpt
@@ -1,5 +1,5 @@
 --TEST--
-Returned CHAR value is not right-trimmed when rtrim option disabled.
+Returned CHAR value is right-trimmed when rtrim option enabled.
 --SKIPIF--
 <?php include("should_run_online_tests.inc"); ?>
 --FILE--
@@ -27,10 +27,10 @@ $params = [
 $f = $c->getFunction("STFC_STRUCTURE");
 $f->setParameterActive('RFCTABLE', false);
 
-$ret = $f->invoke($params, ['rtrim' => false]);
+$ret = $f->invoke($params, ['rtrim' => true]);
 
-var_dump($ret['ECHOSTRUCT']['RFCDATA1'] == str_pad('1DATA1', 50, ' ', STR_PAD_RIGHT));
-var_dump($ret['ECHOSTRUCT']['RFCDATA2'] == str_pad('DATA222', 50, ' ', STR_PAD_RIGHT));
+var_dump($ret['ECHOSTRUCT']['RFCDATA1'] == '1DATA1');
+var_dump($ret['ECHOSTRUCT']['RFCDATA2'] == 'DATA222');
 
 --EXPECT--
 bool(true)