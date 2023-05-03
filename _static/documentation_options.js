e09b6cbfd78c0bca680988dff7c9988b92a4a835
Gregor Kralik <g.kralik@gmail.com>
2023-05-03 18:31:48 +0200

    updated docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0-beta2',
+    VERSION: '2.0.0-beta3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
