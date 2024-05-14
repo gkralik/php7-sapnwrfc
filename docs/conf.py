697270f1a5e1067b82247a07ad819c0e566fd345
Gregor Kralik <g.kralik@gmail.com>
2024-05-14 13:32:36 +0200

    Fix sphinx configuration
    Set language, fix copyright years

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'php7-sapnwrfc'
-copyright = '2026 - 2024 Gregor Kralik'
+copyright = '2016 - 2024 Gregor Kralik'
 author = 'Gregor Kralik'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -63,7 +63,7 @@ release = '2.0.0-beta3'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
